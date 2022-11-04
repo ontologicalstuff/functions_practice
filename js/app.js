30083635cd1f287a02e195de728dc2ecf9d15191
ontologicalstuff <saorsa@tutamail.com>
2022-11-03 20:07:09 -0700

    finish calculate

--- js/app.js
+++ js/app.js
@@ -26,3 +26,30 @@ const reverseWordOrder = wordString => {
 
 console.log(reverseWordOrder('Ishmael me Call'));
 console.log(reverseWordOrder('I use Lâncome on my comb'));
+
+// 3. calculate
+// Write a function called calculate.
+
+// This function should take three arguments: two numbers and a string.
+
+// Name the parameters num1, num2, and operation.
+
+// If if the function is called with the third argument as “add”, it should return the sum of num1 and num2.
+
+// If if the function is called with the third argument as “sub”, it should return return num1 minus num2.
+
+// Do the same thing for multiplication “mult”, division “div”, and exponent “exp” (where num2 is the exponent of num1).
+
+
+
+const calculate = (num1, num2, string) => {
+    if (string === 'add') return num1 + num2;
+    if (string === 'sub') return num1 - num2;
+    if (string === 'mult') return num1 * num2;
+    if (string === 'div') return num1 / num2;
+    if (string === 'exp') return num1 ** num2;
+}
+
+console.log(calculate(4, 3, "sub")); // => 1
+console.log(calculate(4, 3, "exp"));// => 64
+
