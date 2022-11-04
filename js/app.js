d74bb39cac6a3da607ccba0047e7c71fc383beb8
ontologicalstuff <saorsa@tutamail.com>
2022-11-03 19:50:04 -0700

    finish printGreeting()

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// 1. printGreeting
+// Write a function called printGreeting with a parameter name that returns a greeting with the argument interpolated into the greeting.
+
+
+
+const printGreeting = name => {return `Hello there, ${name}`}
+
+console.log(printGreeting("Slimer"));
+// => Hello there, Slimer!
